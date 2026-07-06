b0f8890e4259f0de564a397545592f39677e2cf8
kaldiroglu <96393440+kaldiroglu@users.noreply.github.com>
2026-07-06 16:46:36 +0300

    Some modifications.

--- src/dev/kaldiroglu/dp/structural/adapter/electricity/pluggable/electricity/parameterized/KenyaPowerSource.java
+++ src/dev/kaldiroglu/dp/structural/adapter/electricity/pluggable/electricity/parameterized/KenyaPowerSource.java
@@ -1,11 +1,11 @@
 package dev.kaldiroglu.dp.structural.adapter.electricity.pluggable.electricity.parameterized;
 
 /**
- * <b>Adaptee</b> #1: an Kenyan source with its own, incompatible interface ({@code hakunaMatata}).
+ * <b>Adaptee</b> #1: a Kenyan source with its own, incompatible interface ({@code hakunaMatata}).
  */
 public final class KenyaPowerSource {
 
     public void hakunaMatata() {
-        System.out.println("Kenyan source: switch is pushed in a hakuna matata (110V / 60Hz)");
+        System.out.println("Kenyan source: switch is pushed in a hakuna matata way :)");
     }
 }
